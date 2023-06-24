d9e48a8c59eb2783a85cffc5834612607e3cbc22
vikaster <carrere.yves@gmail.com>
2023-06-24 03:45:30 -0700

    Added a SearchBar + Search Button + Styles

--- Movies/Movies/SearchView.swift
+++ Movies/Movies/SearchView.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State var text : String = ""
     var body: some View {
-        Text("Recherches").font(.largeTitle)
-        
-        
+        VStack(alignment: .center, spacing: 50) {
+            Text("Recherches").font(.custom("Graphik", size: 32)).bold()
+            HStack{
+                TextField("Rechercher un film, un acteur, une actrice...", text:$text)
+                    .font(.custom("Graphik compact", size: 14))
+                    .padding([.top, .leading], 10)
+                    .textSelection(.enabled)
+                    .background(.white)
+                    .cornerRadius(50)
+                    .frame(maxWidth: .infinity, minHeight:22, maxHeight:22)
+            }
+            .padding()
+            .frame(maxWidth: 348, minHeight: 48)
+            .cornerRadius(10).background(.gray)
+            .padding(.bottom, 10)
+                
+            Button(action: {
+                
+            }, label: {
+                Text("Valider")
+                    .foregroundColor(.gray)
+                    .font(.custom("Graphik compact", size: 24))
+        }).buttonStyle(.bordered)
+        }
     }
 }
 
